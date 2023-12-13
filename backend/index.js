30e8d363773ffa050152f16b3d05257627ab1846
Babu rao sanivada <baburao.sanivada10@gmail.com>
2023-12-13 07:17:29 +0530

    Added MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -7,6 +8,9 @@ const app = express();
 app.get("/", (req, res) => {
   return res.status(205).send("Welcome to BookStore");
 });
+
 app.listen(PORT, () => {
   console.log(`App is listening to port : ${PORT}`);
 });
+
+mongoose.connect();
